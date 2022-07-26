99d967baa3d8d5bd307c5cda7ee09e8f9ffbf6c0
Eric <eric.sun.ecs92@yale.edu>
2022-07-26 03:03:47 -0400

    first commit

---
+++ models/posts.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
---
+++ server.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/users");
+const Post = require("./models/posts");
+
+// const users = [
+//   { id: 1, name: "User 1" },
+//   { id: 2, name: "User 2" },
+//   { id: 3, name: "User 3" },
+//   { id: 4, name: "User 4" },
+//   { id: 5, name: "User 5" },
+//   { id: 6, name: "User 6" },
+//   { id: 7, name: "User 7" },
+//   { id: 8, name: "User 8" },
+//   { id: 9, name: "User 9" },
+//   { id: 10, name: "User 10" },
+//   { id: 11, name: "User 11" },
+//   { id: 12, name: "User 12" },
+//   { id: 13, name: "User 13" },
+// ];
+
+// const posts = [
+//   { id: 1, name: "Post 1" },
+//   { id: 2, name: "Post 2" },
+//   { id: 3, name: "Post 3" },
+//   { id: 4, name: "Post 4" },
+//   { id: 5, name: "Post 5" },
+//   { id: 6, name: "Post 6" },
+//   { id: 7, name: "Post 7" },
+//   { id: 8, name: "Post 8" },
+//   { id: 9, name: "Post 9" },
+//   { id: 10, name: "Post 10" },
+//   { id: 11, name: "Post 11" },
+//   { id: 12, name: "Post 12" },
+//   { id: 13, name: "Post 13" },
+// ];
+
+mongoose.connect("mongodb://localhost/pagination");
+const db = mongoose.connection;
+db.once("open", async () => {
+  if ((await User.countDocuments().exec()) <= 0) {
+    Promise.all([
+      User.create({ name: "User 1" }),
+      User.create({ name: "User 2" }),
+      User.create({ name: "User 3" }),
+      User.create({ name: "User 4" }),
+      User.create({ name: "User 5" }),
+      User.create({ name: "User 6" }),
+      User.create({ name: "User 7" }),
+      User.create({ name: "User 8" }),
+      User.create({ name: "User 9" }),
+      User.create({ name: "User 10" }),
+      User.create({ name: "User 11" }),
+      User.create({ name: "User 12" }),
+      User.create({ name: "User 13" }),
+      User.create({ name: "User 14" }),
+      User.create({ name: "User 15" }),
+      User.create({ name: "User 16" }),
+    ]).then(() => console.log("added users"));
+  } else if ((await Post.countDocuments()) <= 0) {
+    Promise.all([
+      Post.create({ name: "Post 1" }),
+      Post.create({ name: "Post 2" }),
+      Post.create({ name: "Post 3" }),
+      Post.create({ name: "Post 4" }),
+      Post.create({ name: "Post 5" }),
+      Post.create({ name: "Post 6" }),
+    ]).then(() => console.log("posts added"));
+  }
+});
+
+app.get("/users", paginatedResults(User), (req, res) => {
+  res.json(res.paginatedResults);
+});
+
+app.get("/posts", paginatedResults(Post), (req, res) => {
+  res.json(res.paginatedResults);
+});
+
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.satus(500).json({ message: e.message });
+    }
+  };
+}
+
+app.listen(3000);
